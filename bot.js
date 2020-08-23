fa1a08ce5d7e97ee60b062718de4d3f736f0494f
mskhh0505 <66984236+mskhh0505@users.noreply.github.com>
2020-08-23 17:24:47 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ login()
         console.log(`login error: ${error}`)
     });
 client.on('ready', () => {
-    client.user.setActivity('Looking at SeohyunCore')
+    client.user.setActivity('ㅁ')
     console.log(`ready ${client.guilds.size} servers.`)
 });
 
