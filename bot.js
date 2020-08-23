96ec4ed1c7409d12edc21bad112305effcc18472
mskhh0505 <66984236+mskhh0505@users.noreply.github.com>
2020-08-23 19:02:42 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const cookie = process.env.cookie;
 const prefix = "!";
 const groupid = 4483539;
 const maximumRank = 9;
-const data = nbx.getPlayerInfo(userId)
+const data = nbx.getPlayerInfo(data)
 const more = nbx.getBlurb(data.id)
 client.login(token)
 function login(){
