c300c6309e570e845dcf7ebbaafa89a13fa0aa20
mskhh0505 <66984236+mskhh0505@users.noreply.github.com>
2020-08-23 18:51:53 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ login()
         console.log(`login error: ${error}`)
     });
 client.on('ready', () => {
-    client.user.setActivity('ㅁㅁ')
+    client.user.setActivity('ㅁㅁㅇㅁㄴ')
     console.log(`ready ${client.guilds.size} servers.`)
 });
 
@@ -36,10 +36,8 @@ client.on('message', (message) => {
             if (message.member.roles.find(role => role.name === "Bot access")){
                 nbx.getIdFromUsername(username)
                 .then(function(id){
-                nbx.getPlayerInfo(data, id)
-                .then(function(data){
                 nbx.getRankInGroup(groupid, id)
-                    .then(function(rank){
+                .then(function(rank){
                         if (maximumRank <= rank){
                             message.reply('봇의 권한보다 높은 사람을 내릴 수 없습니다.')
                         } else{
