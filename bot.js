36f6bf38e384862aaaab023881cd48948573dd91
mskhh0505 <66984236+mskhh0505@users.noreply.github.com>
2020-08-23 18:40:45 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const cookie = process.env.cookie
 const prefix = "!";
 const groupid = 4483539;
 const maximumRank = 9;
-const more = await nbx.getBlurb(data.id)
+const more = nbx.getBlurb(data.id)
 client.login(token)
 function login(){
     return nbx.cookieLogin(cookie);
