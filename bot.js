4061db1549a5fc532f533cd1767372870951c524
mskhh0505 <66984236+mskhh0505@users.noreply.github.com>
2020-08-25 13:27:52 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,3 +110,50 @@ client.on('message', (message) => {
         }
     }
 });
+        // listening event for the search command to receive a prompt to seacrh
+client.on("message", message => {
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    
+    if (command === "정보") {
+      let username = args[0]
+      
+      // if a username is supplied successfully
+      if (username) {
+          nbx.getIdFromUsername(username).then(id => {
+            
+            // if an identity is found under the username then continue collecting the rest of the data
+            // sadly this means you can't search for banned users. f in the chat. maybe try using older apis
+            // yes, i just did c# styled bracketing, do not mind me trying to bless your eyes
+            
+            if (id) 
+  
+            {
+              // next conditio
+              nbx.getPlayerInfo(parseInt(id)).then(function(info) 
+              {
+                // dates.. um. go try get a pear or a grape instead.
+                let date = new Date(info.joinDate)
+                
+                // create new embed and establish some settings for it, tasty.
+                let embed = new discord.RichEmbed()
+                .setColor("#f9ae00") // sets the color of the embed
+                .setURL(`https://roblox.com/users/${id}/profile`) // base link, changed by the variables 'id'
+                .setTimestamp()
+                .setThumbnail(`https://www.roblox.com/bust-thumbnail/image?userId=${id}&width=420&height=420&format=png`) // gets the roblox profile picture
+                .addField("유저 닉네임", `[${username}](https://www.roblox.com/users/${id}/profile)`) // everything in the embed is undefined, therefore can be changed by the variables
+                .addField("유저 아이디", id || '아무것도 없습니다.', true)
+                .addField("설명", info.blurb || '아무 것도 없습니다.', true)
+                .addField("소개", info.status || '아무것도 없습니다.', true)
+                .addField("계정 일 수", `${info.age} days old` || 'Unresolvable')
+                message.channel.send({embed})
+              })
+            }
+          // but what if the player is banned, or doesn't even exist?
+          // houston, we have a problem.
+          }).catch(function (err) {
+           message.channel.send("유저 이름을 찾을 수 없습니다 확인 후 재 입력해주세요.") // catching error
+         });  
+      } else { message.channel.send("양식에 맞춰 입력해주세요. !정보 닉네임") }
+    }
+  });
